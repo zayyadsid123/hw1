4f3b41f4ccc1e602e5aefb24a4f0e16a2eed1fc9
Zayyad Siddiqui <zayyad.siddiqui@kellogg.northwestern.edu>
2025-04-17 03:25:26 +0000

    initial data model, db structure, and movies table creation

--- kmdb.sql
+++ kmdb.sql
@@ -112,14 +112,73 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors; 
+DROP TABLE IF EXISTS roles;   
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released INTEGER,
+  MPAA_rating TEXT,
+  studio_name TEXT
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id INTEGER,
+  actor_id INTEGER,
+  character_full_name TEXT
+);
+
+
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
-
+INSERT INTO movies(
+  title,
+  year_released,
+  MPAA_rating,
+  studio_name
+) VALUES (
+    "Batman Begins",
+    "2005", 
+    "PG-13",
+    "Warner Bros."
+);
+INSERT INTO movies(
+  title,
+  year_released,
+  MPAA_rating,
+  studio_name
+) VALUES (
+    "The Dark Knight",
+    "2008",
+    "PG-13",
+    "Warner Bros."
+);
+INSERT INTO movies(
+  title,
+  year_released,
+  MPAA_rating,
+  studio_name
+) VALUES (
+    "The Dark Knight Rises",
+    "2012",
+    "PG-13",
+    "Warner Bros."
+);
+                  
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
